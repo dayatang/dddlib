0076fad6435dbb44bdd0b804ea18a6d4bcd23361
yyang <gdyangyu@gmail.com>
2013-05-26 21:13:08 +0800

    修改测试

--- dsrouter2/src/test/java/com/dayatang/dsrouter/dscreator/C3P0DataSourceCreatorTest.java
+++ dsrouter2/src/test/java/com/dayatang/dsrouter/dscreator/C3P0DataSourceCreatorTest.java
@@ -1,7 +1,6 @@
 package com.dayatang.dsrouter.dscreator;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
 
 
 import javax.sql.DataSource;
@@ -11,25 +10,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.dayatang.dsrouter.Constants;
-import com.dayatang.configuration.Configuration;
-import com.dayatang.configuration.ConfigurationFactory;
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-
 
 public class C3P0DataSourceCreatorTest {
 
-	private AbstractDataSourceCreator instance;
-	private DbType urlTranslator; 
-	private Configuration configuration = new ConfigurationFactory().fromClasspath(Constants.DB_CONF_FILE);
-	private ComboPooledDataSource dataSource;
+	private C3P0DataSourceCreator instance;
 	
 	@Before
 	public void setUp() throws Exception {
-		urlTranslator = mock(DbType.class);
 		instance = new C3P0DataSourceCreator();
-		dataSource = new ComboPooledDataSource();
-		instance.setDataSource(dataSource);
 	}
 
 	@After
@@ -39,10 +27,9 @@ public class C3P0DataSourceCreatorTest {
 	@Test
 	public void createDataSourceForTenant() throws Exception {
 		String tenant = "abcd";
-		String url = "jdbc:mysql://localhost:3306/testdb_abc";
-		when(urlTranslator.translateUrl(tenant, configuration.getProperties())).thenReturn(url);
+		String url = "jdbc:mysql://localhost:3306/?useUnicode=true&characterEncoding=utf-8";
 		DataSource result = instance.createDataSourceForTenant(tenant);
-		assertSame(dataSource, result);
+		//assertSame(dataSource, result);
 		assertEquals("com.mysql.jdbc.Driver", BeanUtils.getProperty(result, "driverClass"));
 		assertEquals(url, BeanUtils.getProperty(result, "jdbcUrl"));
 		//assertEquals("root", BeanUtils.getProperty(result, "user"));
