c2a6eb30c736806aa580dedb58e5f89dc7b90881
lingen.liu <lingen.liu@gmail.com>
2013-08-07 16:42:56 +0800

    ws cliet优化

---
+++ koala-bpm/koala-jbpm-ws-client/src/main/java/org/openkoala/jbpm/wsclient/ProcessInstanceVO.java
@@ -0,0 +1,252 @@
+
+package org.openkoala.jbpm.wsclient;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for processInstanceVO complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="processInstanceVO">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="createDate" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="creater" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="data" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="lastUpdateDate" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="parentProcessInstanceId" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *         &lt;element name="processId" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="processInstanceId" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *         &lt;element name="processName" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="status" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "processInstanceVO", propOrder = {
+    "createDate",
+    "creater",
+    "data",
+    "lastUpdateDate",
+    "parentProcessInstanceId",
+    "processId",
+    "processInstanceId",
+    "processName",
+    "status"
+})
+public class ProcessInstanceVO {
+
+    protected String createDate;
+    protected String creater;
+    protected String data;
+    protected String lastUpdateDate;
+    protected long parentProcessInstanceId;
+    protected String processId;
+    protected long processInstanceId;
+    protected String processName;
+    protected int status;
+
+    /**
+     * Gets the value of the createDate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    /**
+     * Sets the value of the createDate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCreateDate(String value) {
+        this.createDate = value;
+    }
+
+    /**
+     * Gets the value of the creater property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCreater() {
+        return creater;
+    }
+
+    /**
+     * Sets the value of the creater property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCreater(String value) {
+        this.creater = value;
+    }
+
+    /**
+     * Gets the value of the data property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getData() {
+        return data;
+    }
+
+    /**
+     * Sets the value of the data property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setData(String value) {
+        this.data = value;
+    }
+
+    /**
+     * Gets the value of the lastUpdateDate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    /**
+     * Sets the value of the lastUpdateDate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLastUpdateDate(String value) {
+        this.lastUpdateDate = value;
+    }
+
+    /**
+     * Gets the value of the parentProcessInstanceId property.
+     * 
+     */
+    public long getParentProcessInstanceId() {
+        return parentProcessInstanceId;
+    }
+
+    /**
+     * Sets the value of the parentProcessInstanceId property.
+     * 
+     */
+    public void setParentProcessInstanceId(long value) {
+        this.parentProcessInstanceId = value;
+    }
+
+    /**
+     * Gets the value of the processId property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getProcessId() {
+        return processId;
+    }
+
+    /**
+     * Sets the value of the processId property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setProcessId(String value) {
+        this.processId = value;
+    }
+
+    /**
+     * Gets the value of the processInstanceId property.
+     * 
+     */
+    public long getProcessInstanceId() {
+        return processInstanceId;
+    }
+
+    /**
+     * Sets the value of the processInstanceId property.
+     * 
+     */
+    public void setProcessInstanceId(long value) {
+        this.processInstanceId = value;
+    }
+
+    /**
+     * Gets the value of the processName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getProcessName() {
+        return processName;
+    }
+
+    /**
+     * Sets the value of the processName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setProcessName(String value) {
+        this.processName = value;
+    }
+
+    /**
+     * Gets the value of the status property.
+     * 
+     */
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     * Sets the value of the status property.
+     * 
+     */
+    public void setStatus(int value) {
+        this.status = value;
+    }
+
+}
