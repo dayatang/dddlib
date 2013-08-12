0992f20014cb627dfd0f8ea2dd9d1e24761b77ac
lingen.liu <lingen.liu@gmail.com>
2013-08-12 10:24:45 +0800

    给cache模块加入单元测试

---
+++ koala-commons/koala-commons-cache/src/test/java/org/openkoala/koalacommons/cache/TestEhCacheImpl.java
@@ -0,0 +1,55 @@
+package org.openkoala.koalacommons.cache;
+
+import org.junit.Test;
+import org.openkoala.framework.cache.EhCacheImpl;
+
+import com.dayatang.cache.Cache;
+import com.dayatang.utils.Assert;
+
+/**
+ * 针对Koala的ehcache实现做测试
+ * @author lingen lingen.liu@gmail.com
+ *
+ */
+public class TestEhCacheImpl {
+
+	
+	/**
+	 * 在ehcache.xml中配置指定cache名称进行cache
+	 */
+	@Test
+	public void testCache(){
+		Cache cache = new EhCacheImpl("sampleCache");
+		cache.put("KEY", "VALUE");
+		String value = (String) cache.get("KEY");
+		Assert.isTrue(value.equals("VALUE"));
+		
+		boolean exists = cache.isKeyInCache("KEY");
+		Assert.isTrue(exists);
+		
+		cache.remove("KEY");
+		exists = cache.isKeyInCache("KEY");
+		Assert.isTrue(!exists);
+	}
+	
+	/**
+	 * 使用参数手动构建一个cache
+	 */
+	@Test
+	public void testCache2(){
+		Cache cache = new EhCacheImpl("mycache",100000,true,true,1200000,200000);
+		cache.put("KEY", "VALUE");
+		String value = (String) cache.get("KEY");
+		Assert.isTrue(value.equals("VALUE"));
+		
+		boolean exists = cache.isKeyInCache("KEY");
+		Assert.isTrue(exists);
+		
+		cache.remove("KEY");
+		exists = cache.isKeyInCache("KEY");
+		Assert.isTrue(!exists);
+		
+	}
+	
+	
+}
