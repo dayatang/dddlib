923f4628d3678552ae83676507e3ea9c01478bcc
lambo <lambo@lambo-PC>
2013-08-15 12:07:30 +0800

    .

--- koala-gqc/koala-gqc-applicationImpl/src/test/java/org/openkoala/gqc/application/impl/DataSourceApplicationImplTest.java
+++ koala-gqc/koala-gqc-applicationImpl/src/test/java/org/openkoala/gqc/application/impl/DataSourceApplicationImplTest.java
@@ -1,27 +1,36 @@
 package org.openkoala.gqc.application.impl;
 
-import static org.junit.Assert.fail;
-
-import java.util.ArrayList;
-import java.util.List;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.openkoala.gqc.application.DataSourceApplication;
 import org.openkoala.gqc.core.domain.DataSource;
+
+import com.dayatang.domain.EntityRepository;
 
 public class DataSourceApplicationImplTest {
 	
-	private static List<DataSource> list;
+	private DataSourceApplication instance = new DataSourceApplicationImpl();
+	
+	@Mock
+	private EntityRepository repository;
+	
+	private DataSource dataSource;
+	
+//	private static List<DataSource> list;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		list = new ArrayList<DataSource>();
-		for(int i=0; i<5; i++){
-			
-		}
+//		list = new ArrayList<DataSource>();
+//		for(int i=0; i<5; i++){
+//			
+//		}
 	}
 
 	@AfterClass
@@ -39,57 +48,57 @@ public class DataSourceApplicationImplTest {
 
 	@Test
 	public void testGetDataSource() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetDataSourceVoByDataSourceId() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSaveDataSource() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testUpdateDataSource() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveDataSource() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveDataSources() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testFindAllDataSource() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testFindAllTable() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testFindAllColumn() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPageQueryDataSource() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testTestConnection() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 }
