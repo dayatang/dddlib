913aad901265e853b5537bded7bca8786f458f60
zyb <zhuyuanbiao2013@gmail.com>
2013-08-20 14:30:15 +0800

    Add package-inf for security.

---
+++ koala-security/koala-security-core/src/main/java/org/openkoala/koala/auth/core/domain/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 权限系统领域层
+ * @author zyb <a href="mailto:zhuyuanbiao2013@gmail.com">zhuyuanbiao2013@gmail.com</a>
+ * @since Aug 20, 2013 2:24:56 PM
+ */
+package org.openkoala.koala.auth.core.domain;
