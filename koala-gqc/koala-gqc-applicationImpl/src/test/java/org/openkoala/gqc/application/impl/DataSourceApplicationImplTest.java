e46743d6fc3c2c1b4e957153f746fb21a86c426c
yeren <shibinlambo@gamil.com>
2013-08-29 17:31:06 +0800

    通用查询单元测试

--- koala-gqc/koala-gqc-applicationImpl/src/test/java/org/openkoala/gqc/application/impl/DataSourceApplicationImplTest.java
+++ koala-gqc/koala-gqc-applicationImpl/src/test/java/org/openkoala/gqc/application/impl/DataSourceApplicationImplTest.java
@@ -2,20 +2,20 @@ package org.openkoala.gqc.application.impl;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
-import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.dbcp.BasicDataSource;
 import org.hamcrest.core.IsNull;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.internal.matchers.StringContains;
 import org.junit.runner.RunWith;
+import org.logicalcobwebs.proxool.ProxoolDataSource;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.openkoala.gqc.core.domain.DataSource;
@@ -24,8 +24,8 @@ import org.openkoala.gqc.vo.DataSourceVO;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.springframework.test.AssertThrows;
 
+import com.dayatang.IocException;
 import com.dayatang.domain.EntityRepository;
 import com.dayatang.domain.InstanceFactory;
 import com.dayatang.querychannel.service.QueryChannelService;
@@ -44,10 +44,22 @@ public class DataSourceApplicationImplTest {
 	@Mock
 	private EntityRepository repository;
 	
+	@Mock
+	private Connection conn;
+	
+	private ProxoolDataSource ds;
+	
 	/**
 	 * 创建实例
 	 */
 	private DataSource dataSource;
+	
+//	private long idExist = 2L;
+//	private long idNotExist = 100L;
+//	
+//	private String dataSourceIdNotExist = "notInDb";
+	
+	private final String jpqlForDataSourceId = " select _dataSource from DataSource _dataSource  where _dataSource.dataSourceId = ? ";
 	
 	@Before
 	public void setUp() throws Exception {
@@ -56,123 +68,87 @@ public class DataSourceApplicationImplTest {
 		instance.setQueryChannelService(queryChannel);
 		//测试静态方法
 		PowerMockito.mockStatic(InstanceFactory.class);
+		dataSource = new DataSource();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		DataSource.setRepository(null);
+		dataSource = null;
 	}
 
 	@Test
-	public void testGetDataSource() {
-		dataSource = createDataSource();
+	public void testGetDataSourceById() {
+		this.mockOneDbRecordExistOfSystemDataSourceById();
+		this.assertResultEqualsById();
 		
-//		when(queryChannel.querySingleResult("sql",new Object[]{dataSource.getId()})).thenReturn(dataSource);
+		this.mockOneDbRecordExistOfCustomDataSourceById();
+		this.assertResultEqualsById();
 		
-		when(repository.get(DataSource.class, dataSource.getId())).thenReturn(dataSource);
+		this.mockOneDbRecordNotExistOfSystemDataSourceById();
+		this.assertResultNotEqualsById();
 		
-		DataSourceVO dataSourceVO = null;
-		try {
-			dataSourceVO = turnToDataSourceVO(dataSource);
-		} catch (Exception e) {
-			fail("实体转换成VO时失败！");
-		}
-		
-		assertThat(instance.getDataSourceVoById(2L), is(dataSourceVO));
+		this.mockOneDbRecordNotExistOfCustomDataSourceById();
+		this.assertResultNotEqualsById();
 	}
-
+	
 	@Test
 	public void testGetDataSourceVoByDataSourceId() {
-		String jpql = " select _dataSource from DataSource _dataSource  where _dataSource.dataSourceId = ? ";
-		dataSource = createDataSource();
-		//返回非空的分支
-		when(queryChannel.querySingleResult(jpql, new Object[] { dataSource.getDataSourceId() })).thenReturn(dataSource);
-		DataSourceVO dataSourceVO = null;
-		try {
-			dataSourceVO = turnToDataSourceVO(dataSource);
-		} catch (Exception e) {
-			fail("实体转换成VO时失败！");
-		}
-		assertThat(instance.getDataSourceVoByDataSourceId(dataSource.getDataSourceId()), is(dataSourceVO));
+		this.mockOneDbRecordExistOfSystemDataSourceByDataSourceId();
+		this.assertResultEqualsByDataSourceId();
 
-		//返回空的分支
-		when(queryChannel.querySingleResult(jpql, new Object[] { "notInDb" })).thenReturn(null);
-		assertThat(instance.getDataSourceVoByDataSourceId( "notInDb" ), not(dataSourceVO));
+		this.mockOneDbRecordExistOfCustomDataSourceByDataSourceId();
+		this.assertResultEqualsByDataSourceId();
+
+		this.mockOneDbRecordNotExistOfSystemDataSourceByDataSourceId();
+		this.assertResultNotEqualsByDataSourceId();
+
+		this.mockOneDbRecordNotExistOfCustomDataSourceByDataSourceId();
+		this.assertResultNotEqualsByDataSourceId();
 	}
 
 	@Test
 	public void testSaveDataSource() {
-		String jpql = " select _dataSource from DataSource _dataSource  where _dataSource.dataSourceId = ? ";
-		dataSource = createDataSource();
-		/**返回数据源ID已存在的分支*/
-		when(queryChannel.querySingleResult(jpql, new Object[] { dataSource.getDataSourceId() })).thenReturn(dataSource);
-		DataSourceVO dataSourceVO = null;
-		try {
-			dataSourceVO = turnToDataSourceVO(dataSource);
-		} catch (Exception e) {
-			fail("实体转换成VO时失败！");
-		}
+		this.mockOneDbRecordExistOfSystemDataSourceByDataSourceId();
+		DataSourceVO dataSourceVO = turnToDataSourceVO();
 		assertThat(instance.saveDataSource(dataSourceVO), is("该数据源ID已存在"));
 		
-		/**返回数据源ID不存在的分支*/
-		//初始化系统数据源，数据源id不存在
-		dataSource = initSystemDataSourceAndDataSourceIdNotExist();
-		try {
-			dataSourceVO = turnToDataSourceVO(dataSource);
-		} catch (Exception e) {
-			fail("实体转换成VO时失败！");
-		}
-		PowerMockito.when(InstanceFactory.getInstance(javax.sql.DataSource.class, dataSource.getDataSourceId())).thenThrow(new RuntimeException("该系统数据源不存在！"));
-		when(queryChannel.querySingleResult(jpql, new Object[] { "dataSourceId1" })).thenReturn(null);
+		this.mockOneDbRecordExistOfCustomDataSourceByDataSourceId();
+		dataSourceVO = turnToDataSourceVO();
+		assertThat(instance.saveDataSource(dataSourceVO), is("该数据源ID已存在"));
+		
+		this.mockOneDbRecordNotExistOfSystemDataSourceByDataSourceId();
+		dataSourceVO = turnToDataSourceVO();
+		PowerMockito.when(InstanceFactory.getInstance(javax.sql.DataSource.class, dataSource.getDataSourceId())).thenThrow(new IocException());
 		assertThat(instance.saveDataSource(dataSourceVO), StringContains.containsString("系统数据源不存在"));
 		
-		//初始化系统数据源，数据源id存在
-		dataSource = initSystemDataSourceAndDataSourceIdExist();
+		this.initSystemDataSourceAndDataSourceIdNotInDbButInSystem();
+		dataSourceVO = turnToDataSourceVO();
+		this.initProxoolDataSource();
+		PowerMockito.when(InstanceFactory.getInstance(javax.sql.DataSource.class, 
+				dataSource.getDataSourceId())).thenReturn(ds);
+		assertNull(instance.saveDataSource(dataSourceVO));
 		try {
-			dataSourceVO = turnToDataSourceVO(dataSource);
-		} catch (Exception e) {
-			fail("实体转换成VO时失败！");
+			ds.getConnection().close();
+		} catch (SQLException e) {
+			fail("连接关闭失败！");
 		}
-		javax.sql.DataSource ds = new BasicDataSource();
-		PowerMockito.when(InstanceFactory.getInstance(javax.sql.DataSource.class, dataSource.getDataSourceId())).thenReturn(ds);
-		when(queryChannel.querySingleResult(jpql, new Object[] { dataSource.getDataSourceId() })).thenReturn(null);
-		assertThat(instance.saveDataSource(dataSourceVO), StringContains.containsString("获取系统数据源失败"));
 		
-		//初始化自定义数据源，不能连接
-		dataSource = initCustomDataSourceCannotConnect();
+		this.initCustomDataSourceCannotConnect();
+		dataSourceVO = turnToDataSourceVO();
 		when(repository.save(dataSource)).thenReturn(dataSource);
-		try {
-			dataSourceVO = turnToDataSourceVO(dataSource);
-		} catch (Exception e) {
-			fail("实体转换成VO时失败！");
-		}
-		assertThat(instance.saveDataSource(dataSourceVO),IsNull.nullValue());
-		
-		//初始化自定义数据源，能连接
-		dataSource = initCustomDataSourceCanConnect();
-		when(repository.save(dataSource)).thenReturn(dataSource);
-		try {
-			dataSourceVO = turnToDataSourceVO(dataSource);
-		} catch (Exception e) {
-			fail("实体转换成VO时失败！");
-		}
 		assertThat(instance.saveDataSource(dataSourceVO),IsNull.nullValue());
 	}
-
+	
 	@Test
 	public void testUpdateDataSource() {
-		dataSource = createDataSource();
-		when(repository.get(DataSource.class,dataSource.getId())).thenReturn(dataSource);
+		DataSourceVO dataSourceVO = this.createDataSourceVO();
 		
-		DataSourceVO dataSourceVO = null;
-		try {
-			dataSourceVO = turnToDataSourceVO(dataSource);
-		} catch (Exception e) {
-			fail("实体转换成VO时失败！");
-		}
-		dataSource = createDataSourceToUpdate();
-		//when(instance.updateDataSource(dataSourceVO);)
+		this.initSystemDataSourceAndDataSourceIdExist();
+		when(repository.get(DataSource.class,dataSourceVO.getId())).thenReturn(dataSource);
 		
+		instance.updateDataSource(dataSourceVO);
+		assertTrue(true);
 	}
 
 	@Test
@@ -217,102 +193,225 @@ public class DataSourceApplicationImplTest {
 	
 	/**
 	 * 实体转换成vo
-	 * @param dataSource
+	 * @param
 	 * @return
-	 * @throws IllegalAccessException
-	 * @throws InvocationTargetException
 	 */
-	private DataSourceVO turnToDataSourceVO(DataSource dataSource) throws Exception {
+	private DataSourceVO turnToDataSourceVO(){
+		try {
+			DataSourceVO dataSourceVO = new DataSourceVO();
+			BeanUtils.copyProperties(dataSourceVO, dataSource);
+			dataSourceVO.setDataSourceTypeDesc(dataSourceVO.getDataSourceType().getDescription());
+			return dataSourceVO;
+		} catch (Exception e) {
+			fail("实体转换成VO时失败！");
+			return null;
+		}
+	}
+	
+	/**
+	 * 创建VO对象
+	 * @return
+	 */
+	private DataSourceVO createDataSourceVO(){
 		DataSourceVO dataSourceVO = new DataSourceVO();
-		BeanUtils.copyProperties(dataSourceVO, dataSource);
-		dataSourceVO.setDataSourceTypeDesc(dataSourceVO.getDataSourceType().getDescription());
+		dataSourceVO.setId(1L);
+		dataSourceVO.setDataSourceType(DataSourceType.SYSTEM_DATA_SOURCE);
+		dataSourceVO.setJdbcDriver("a");
+		dataSourceVO.setConnectUrl("a");
+		dataSourceVO.setUsername("a");
+		dataSourceVO.setPassword("a");
 		return dataSourceVO;
 	}
 	
 	/**
-	 * 模拟一个实体
+	 * 初始化系统数据源，数据源id不存在于数据库
 	 * @return
 	 */
-	private DataSource createDataSource(){
-		DataSource dataSource = new DataSource();
-		dataSource.setId(2L);
-		dataSource.setDataSourceId("dataSourceId1");
-		dataSource.setDataSourceType(DataSourceType.CUSTOM_DATA_SOURCE);
-		dataSource.setJdbcDriver("driver1");
-		dataSource.setConnectUrl("connectUrl1");
-		dataSource.setUsername("username1");
-		dataSource.setPassword("password1");
-		return dataSource;
-	}
-	
-	/**
-	 * 模拟一个实体
-	 * @return
-	 */
-	private DataSource createDataSourceToUpdate(){
-		DataSource dataSource = new DataSource();
-		dataSource.setId(2L);
-		dataSource.setDataSourceId("dataSourceId2");
-		dataSource.setDataSourceType(DataSourceType.CUSTOM_DATA_SOURCE);
-		dataSource.setJdbcDriver("driver2");
-		dataSource.setConnectUrl("connectUrl2");
-		dataSource.setUsername("username2");
-		dataSource.setPassword("password2");
-		return dataSource;
-	}
-	
-	/**
-	 * 初始化系统数据源，数据源id不存在
-	 * @return
-	 */
-	private DataSource initSystemDataSourceAndDataSourceIdNotExist() {
-		DataSource dataSource = new DataSource();
+	private void initSystemDataSourceAndDataSourceIdNotExist() {
+		dataSource.setId(1L);
+		dataSource.setDataSourceId("aaa");
 		dataSource.setDataSourceType(DataSourceType.SYSTEM_DATA_SOURCE);
-		dataSource.setDataSourceId("aaabbb");
-		
-		return dataSource;
+		dataSource.setJdbcDriver("aaa");
+		dataSource.setConnectUrl("aaa");
+		dataSource.setUsername("aaa");
+		dataSource.setPassword("aaa");
 	}
 	
 	/**
-	 * 初始化系统数据源，数据源id存在
+	 * 初始化系统数据源，数据源id存在于数据库
 	 * @return
 	 */
-	private DataSource initSystemDataSourceAndDataSourceIdExist() {
-		DataSource dataSource = new DataSource();
-		dataSource.setDataSourceType(DataSourceType.SYSTEM_DATA_SOURCE);
+	private void initSystemDataSourceAndDataSourceIdNotInDbButInSystem() {
 		dataSource.setDataSourceId("dataSource_gqc");
-		
-		return dataSource;
+		dataSource.setDataSourceType(DataSourceType.SYSTEM_DATA_SOURCE);
+	}
+	
+	/**
+	 * 初始化系统数据源，数据源id存在于数据库
+	 * @return
+	 */
+	private void initSystemDataSourceAndDataSourceIdExist() {
+		dataSource.setId(2L);
+		dataSource.setDataSourceId("bbb");
+		dataSource.setDataSourceType(DataSourceType.SYSTEM_DATA_SOURCE);
+		dataSource.setJdbcDriver("bbb");
+		dataSource.setConnectUrl("bbb");
+		dataSource.setUsername("bbb");
+		dataSource.setPassword("bbb");
 	}
 	
 	/**
 	 * 初始化自定义数据源，不能连接
 	 * @return
 	 */
-	private DataSource initCustomDataSourceCannotConnect() {
-		DataSource dataSource = new DataSource();
+	private void initCustomDataSourceCannotConnect() {
+		dataSource.setId(3L);
+		dataSource.setDataSourceId("ccc");
 		dataSource.setDataSourceType(DataSourceType.CUSTOM_DATA_SOURCE);
-		dataSource.setJdbcDriver("a");
-		dataSource.setConnectUrl("a");
-		dataSource.setUsername("a");
-		dataSource.setPassword("a");
-
-		return dataSource;
+		dataSource.setJdbcDriver("ccc");
+		dataSource.setConnectUrl("ccc");
+		dataSource.setUsername("ccc");
+		dataSource.setPassword("ccc");
 	}
 	
 	/**
 	 * 初始化自定义数据源，可以连接
 	 * @return
 	 */
-	private DataSource initCustomDataSourceCanConnect() {
-		DataSource dataSource = new DataSource();
+	private void initCustomDataSourceCanConnect() {
+		dataSource.setId(4L);
+		dataSource.setDataSourceId("ddd");
 		dataSource.setDataSourceType(DataSourceType.CUSTOM_DATA_SOURCE);
-		dataSource.setJdbcDriver("org.h2.Driver");
-		dataSource.setConnectUrl("jdbc:h2:~/db/test");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-
-		return dataSource;
+		dataSource.setJdbcDriver("ddd");
+		dataSource.setConnectUrl("ddd");
+		dataSource.setUsername("ddd");
+		dataSource.setPassword("ddd");
+	}
+	
+	/**
+	 * 模拟查询出一条数据库记录--系统数据源（通过主键id）
+	 * @param dataSource
+	 */
+	private void mockOneDbRecordExistOfSystemDataSourceById(){
+		this.initSystemDataSourceAndDataSourceIdExist();
+		when(repository.get(DataSource.class, dataSource.getId())).thenReturn(dataSource);
+	}
+	
+	/**
+	 * 模拟查询出一条数据库记录--自定义数据源（通过主键id）
+	 * @param dataSource
+	 */
+	private void mockOneDbRecordExistOfCustomDataSourceById(){
+		this.initCustomDataSourceCanConnect();
+		when(repository.get(DataSource.class, dataSource.getId())).thenReturn(dataSource);
+	}
+	
+	/**
+	 * 模拟未查询出一条数据库记录--系统数据源（通过主键id）
+	 * @param dataSource
+	 */
+	private void mockOneDbRecordNotExistOfSystemDataSourceById(){
+		initSystemDataSourceAndDataSourceIdNotExist();
+		when(repository.get(DataSource.class, dataSource.getId())).thenReturn(null);
+	}
+	
+	/**
+	 * 模拟未查询出一条数据库记录--自定义数据源（通过主键id）
+	 * @param dataSource
+	 */
+	private void mockOneDbRecordNotExistOfCustomDataSourceById(){
+		initCustomDataSourceCannotConnect();
+		when(repository.get(DataSource.class, dataSource.getId())).thenReturn(null);
+	}
+	
+	
+	/**
+	 * 模拟查询出一条数据库记录--系统数据源（通过唯一标识dataSourceId）
+	 * @param dataSource
+	 */
+	private void mockOneDbRecordExistOfSystemDataSourceByDataSourceId(){
+		this.initSystemDataSourceAndDataSourceIdExist();
+		when(queryChannel.querySingleResult(jpqlForDataSourceId, 
+				new Object[] { dataSource.getDataSourceId() })).thenReturn(dataSource);
+	}
+	
+	/**
+	 * 模拟查询出一条数据库记录--自定义数据源（通过主键id）
+	 * @param dataSource
+	 */
+	private void mockOneDbRecordExistOfCustomDataSourceByDataSourceId(){
+		this.initCustomDataSourceCanConnect();
+		when(queryChannel.querySingleResult(jpqlForDataSourceId, 
+				new Object[] { dataSource.getDataSourceId() })).thenReturn(dataSource);
+	}
+	
+	/**
+	 * 模拟未查询出一条数据库记录--系统数据源（通过唯一标识dataSourceId）
+	 * @param dataSource
+	 */
+	private void mockOneDbRecordNotExistOfSystemDataSourceByDataSourceId(){
+		initSystemDataSourceAndDataSourceIdNotExist();
+		when(queryChannel.querySingleResult(jpqlForDataSourceId, 
+				new Object[] { dataSource.getDataSourceId() })).thenReturn(null);
+	}
+	
+	/**
+	 * 模拟未查询出一条数据库记录--自定义数据源（通过主键id）
+	 * @param dataSource
+	 */
+	private void mockOneDbRecordNotExistOfCustomDataSourceByDataSourceId(){
+		initCustomDataSourceCannotConnect();
+		when(queryChannel.querySingleResult(jpqlForDataSourceId, 
+				new Object[] { dataSource.getDataSourceId() })).thenReturn(null);
+	}
+	
+	/**
+	 * 断言相等
+	 * @param dsId
+	 * @param dataSourceVO
+	 */
+	private void assertResultEqualsById(){
+		DataSourceVO dataSourceVO = turnToDataSourceVO();
+		assertThat(instance.getDataSourceVoById(dataSource.getId()), is(dataSourceVO));
+	}
+	
+	/**
+	 * 断言相等
+	 * @param dsId
+	 * @param dataSourceVO
+	 */
+	private void assertResultNotEqualsById(){
+		DataSourceVO dataSourceVO = turnToDataSourceVO();
+		assertThat(instance.getDataSourceVoById(dataSource.getId()), not(dataSourceVO));
+	}
+	
+	/**
+	 * 断言相等
+	 * @param dsId
+	 * @param dataSourceVO
+	 */
+	private void assertResultEqualsByDataSourceId(){
+		DataSourceVO dataSourceVO = turnToDataSourceVO();
+		assertThat(instance.getDataSourceVoByDataSourceId(dataSource.getDataSourceId()), is(dataSourceVO));
+	}
+	
+	/**
+	 * 断言相等
+	 * @param dsId
+	 * @param dataSourceVO
+	 */
+	private void assertResultNotEqualsByDataSourceId(){
+		DataSourceVO dataSourceVO = turnToDataSourceVO();
+		assertThat(instance.getDataSourceVoByDataSourceId(dataSource.getDataSourceId()), not(dataSourceVO));
+	}
+	
+	private void initProxoolDataSource(){
+		ds = new ProxoolDataSource();
+		ds.setAlias("test");
+		ds.setDriver("org.h2.Driver");
+		ds.setDriverUrl("jdbc:h2:~/db/koala");
+		ds.setUser("sa");
+		ds.setPassword("");
 	}
 
 }
