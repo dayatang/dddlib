f132e6b0ef4577592a88ae522c5b502e50e6bad0
lingen <lingen.liu@gmail.com>
2013-09-13 10:02:24 +0800

    Koala OpenCIS初始化

---
+++ koala-opencis/koala-opencis-jira/src/main/java/org/openkoala/opencis/jira/TestAPP.java
@@ -0,0 +1,13 @@
+package org.openkoala.opencis.jira;
+
+public class TestAPP {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
