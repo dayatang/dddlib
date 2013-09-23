c7298b40f4063d2b925cfd222fa0caf0d10f01cd
zhaojh221@gmail.com <zhaojh221@gmail.com>
2013-09-23 19:22:37 +0800

    整理package

---
+++ koala-opencis/koala-opencis-trac/src/test/java/org/openkoala/opencis/trac/MockProject.java
@@ -0,0 +1,34 @@
+package org.openkoala.opencis.trac;
+
+import java.util.List;
+
+import org.openkoala.opencis.api.Developer;
+import org.openkoala.opencis.api.Project;
+
+public class MockProject implements Project {
+
+	@Override
+	public String getArtifactId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getProjectName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getProjectPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Developer> getProjectDeveloper() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- koala-opencis/koala-opencis-trac/src/test/java/org/openkoala/opencis/trac/TracCISClientTest.java
+++ koala-opencis/koala-opencis-trac/src/test/java/org/openkoala/opencis/trac/TracCISClientTest.java
@@ -10,7 +10,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openkoala.opencis.api.Project;
-import org.openkoala.opencis.trac.api.TestTracProject;
 
 import com.trilead.ssh2.Connection;
 import com.trilead.ssh2.Session;
