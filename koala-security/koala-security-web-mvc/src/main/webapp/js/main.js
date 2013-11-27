0a3b082422b9fba4e3008ec7a72ca61e261e1c22
wangjianbing <wangjb925@gmail.com>
2013-11-27 23:53:21 +0800

    main.js提取出来

--- koala-security/koala-security-web-mvc/src/main/webapp/js/main.js
+++ koala-security/koala-security-web-mvc/src/main/webapp/js/main.js
@@ -11,7 +11,9 @@ $(function(){
 			$('#sidebar-collapse').hide();
 			sidebar.css('height', 'auto');
 			return;
-		}
+		}else{
+            changeHeight();
+        }
 	});
 	/**
 	 * 根据内容改变高度
@@ -22,7 +24,7 @@ $(function(){
 		var headerHeight = $('.g-head').outerHeight();
 		var windowHeight = $window.height();
 		var bodyHeight = $(document).height();
-		if(bodyHeight >  windowHeight){
+        if(bodyHeight >  windowHeight){
 			windowHeight =  bodyHeight;
 		}
 		var footHeight = $('#footer').outerHeight();
@@ -41,13 +43,11 @@ $(function(){
 				changeHeight();
 			});
 	};
-	loadContent($('#home'), 'pages/welcome.html');
-
-	var firstLevelMenu = $('.first-level-menu');
+    loadContent($('#home'), 'pages/welcome.html');
 	/*
 	* 菜单收缩样式变化
 	 */
-	firstLevelMenu.find('[data-toggle="collapse"]').on('click', function(){
+	$('.first-level-menu').find('[data-toggle="collapse"]').on('click', function(){
 		var $this = $(this);
 		if($this.hasClass('collapsed')){
 			$this.find('i:last').removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-left');
@@ -55,77 +55,29 @@ $(function(){
 			$this.find('i:last').removeClass('glyphicon-chevron-left').addClass('glyphicon-chevron-right');
 		}
 	})
-	$.get('/auth/Menu/findTopMenuByUser.koala').done(function(data){
-        if(data.data.length == 0){
-            var dialog = $('<div class="modal fade"><div class="modal-dialog" style="padding-top:10%;width:400px;"><div class="modal-content">' +
-                ' <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">' +
-                '&times;</button><h4 class="modal-title">菜单初始化</h4></div><div class="modal-body">' +
-                '<p>是否进行菜单初始化？</p></div><div class="modal-footer"><button type="button" class="btn btn-default"' +
-                ' data-dismiss="modal">取消</button><button id="confirm" type="button" class="btn btn-primary">确定</button></div>' +
-                '</div></div></div>');
-            dialog.modal({
-                keyboard: true
-            }).on('hidden.bs.modal', function(){
-                 $(this).remove();
-            }).find('#confirm').on('click', function(){
-                    $.ajax({
-                        type: 'GET',
-                        url:'dbinit',
-                        dataType: 'text'
-                    }).done(function(data){
-                        if(data == 'success'){
-                            dialog.modal('hide');
-                            $('body').message({
-                                type: 'success',
-                                content: '权限初始化成功'
-                            });
-                            setTimeout(function(){
-                            	window.location.reload();
-                            },2000);
-                        }
-                    });
-            });
-        }
-		$.each(data.data, function(){
-			var $li = $('<li><a data-toggle="collapse" href="#menuMark'+this.id+'"><img class="menu-icon" src="'+this.icon+'"></img>&nbsp;'+this.name+'&nbsp;'+
-				'<i class="glyphicon glyphicon-chevron-left"></i></a><ul id="menuMark'+this.id+'" class="second-level-menu"></ul></li>');
-			firstLevelMenu.append($li);
-			renderSubMenu(this.id, $li);
-		});
-	});
-	var renderSubMenu = function(id, $menu){
-		$.get('/auth/Menu/findAllSubMenuByParent.koala?resVO.id='+id).done(function(data){
-				var subMenus = new Array();
-				$.each(data.data, function(){
-					if(this.menuType == "2"){
-                        var $li = $('<li><a data-toggle="collapse" href="#menuMark'+this.id+'"><img class="menu-icon" src="'+this.icon+'"></img>&nbsp;'+this.name+'&nbsp;'+
-                            '<i class="glyphicon glyphicon-chevron-right pull-right" style="position: relative; right: 12px;font-size: 12px;"></i></a><ul id="menuMark'+this.id+'" class="second-level-menu collapse"></ul></li>');
-                        $li.appendTo($menu.find('.second-level-menu:first')).find('a').css('padding-left', parseInt(this.level)*18+'px');
-                        renderSubMenu(this.id, $li);
-                    }else{
-                        var $li = $(' <li class="submenu" data-role="openTab" data-target="'+this.identifier+'" data-title="'+this.name+'" ' +
-                            'data-mark="menuMark'+this.id+'"><a ><img class="menu-icon" src="'+this.icon+'"></img>&nbsp;'+this.name+'</a></li>');
-                        $li.appendTo($menu.find('.second-level-menu:first')).find('a').css('padding-left', parseInt(this.level)*18+'px');
-                    }
-				});
-				$menu.find('li.submenu').on('click', function(){
-						var $this = $(this);
-						clearMenuEffect();
-						$this.addClass('active').parent().closest('li').addClass('active').parent().closest('li').addClass('active');
-						var target = $this.data('target');
-						var title = $this.data('title');
-						var mark = $this.data('mark');
-						if(target && title && mark ){
-							$this.openTab(target, title, mark);
-						}
-					});
-		});
-	};
+	/*
+	 *菜单点击事件
+	 */
+	var $submenu = $('.first-level-menu').find('li');
+	$submenu.on('click', function(){
+		var $this = $(this);
+		if($this.hasClass('active')) {
+			return;
+		}
+		clearMenuEffect();
+		$this.addClass('active').parent().closest('li').addClass('active').parent().closest('li').addClass('active');
+		var target = $this.data('target');
+		var title = $this.data('title');
+		var mark = $this.data('mark');
+		if(target && title && mark ){
+			$this.openTab(target, title, mark);
+		}
+	})
 	/*
 	 * 清除菜单效果
 	 */
 	var clearMenuEffect = function(){
-		$('.first-level-menu').find('li').each(function(){
+		$submenu.each(function(){
 			var $menuLi = $(this);
 			$menuLi.hasClass('active') && $menuLi.removeClass('active').parent().parent().removeClass('active');
 		});
@@ -142,8 +94,8 @@ $(function(){
 	 */
 	$.fn.openTab = function(target, title, mark, id, param){
 		var mainc =   $('.g-mainc');
-		var tabs = mainc.find('ul.nav');
-		var contents =  mainc.find('div.tab-content');
+		var tabs = mainc.find('#navTabs');
+		var contents =  mainc.find('#tabContent');
 		var content = contents.find('#'+mark);
 		if(content.length > 0){
 			content.attr('data-value', id);
@@ -193,13 +145,13 @@ $(function(){
 	});
 	/*
 	 切换用户
-	 */
+	*/
 	self.on('switchUser',function(){
 		window.location.href = "j_spring_security_logout";
 	});
 	/*
-	 注销
-	 */
+	注销
+	*/
 	self.on('loginOut',function(){
 		window.location.href = "j_spring_security_logout";
 	});
--- koala-security/koala-security-web-mvc/src/main/webapp/pages/index.jsp
+++ koala-security/koala-security-web-mvc/src/main/webapp/pages/index.jsp
@@ -65,5 +65,75 @@
 	<script type="text/javascript" src="<c:url value='/lib/z-tree/js/jquery.ztree.all-3.5.min.js'/>"></script>
 	<script type="text/javascript" src="<c:url value='/js/validation.js' />"></script>
 	<script type="text/javascript" src="<c:url value='/js/main.js' />?time=<%=time%>" ></script>
+	<script>
+		$(function(){ 
+			$.get('/auth/Menu/findTopMenuByUser.koala').done(function(data){
+		        if(data.data.length == 0){
+		            var dialog = $('<div class="modal fade"><div class="modal-dialog" style="padding-top:10%;width:400px;"><div class="modal-content">' +
+		                ' <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">' +
+		                '&times;</button><h4 class="modal-title">菜单初始化</h4></div><div class="modal-body">' +
+		                '<p>是否进行菜单初始化？</p></div><div class="modal-footer"><button type="button" class="btn btn-default"' +
+		                ' data-dismiss="modal">取消</button><button id="confirm" type="button" class="btn btn-primary">确定</button></div>' +
+		                '</div></div></div>');
+		            dialog.modal({
+		                keyboard: true
+		            }).on('hidden.bs.modal', function(){
+		                 $(this).remove();
+		            }).find('#confirm').on('click', function(){
+		                    $.ajax({
+		                        type: 'GET',
+		                        url:'dbinit',
+		                        dataType: 'text'
+		                    }).done(function(data){
+		                        if(data == 'success'){
+		                            dialog.modal('hide');
+		                            $('body').message({
+		                                type: 'success',
+		                                content: '权限初始化成功'
+		                            });
+		                            setTimeout(function(){
+		                            	window.location.reload();
+		                            },2000);
+		                        }
+		                    });
+		            });
+		        }
+				$.each(data.data, function(){
+					var $li = $('<li><a data-toggle="collapse" href="#menuMark'+this.id+'"><img class="menu-icon" src="'+this.icon+'"></img>&nbsp;'+this.name+'&nbsp;'+
+						'<i class="glyphicon glyphicon-chevron-left"></i></a><ul id="menuMark'+this.id+'" class="second-level-menu"></ul></li>');
+					$('.first-level-menu').append($li);
+					renderSubMenu(this.id, $li);
+				});
+			});
+			var renderSubMenu = function(id, $menu){
+				$.get('/auth/Menu/findAllSubMenuByParent.koala?resVO.id='+id).done(function(data){
+						var subMenus = new Array();
+						$.each(data.data, function(){
+							if(this.menuType == "2"){
+		                        var $li = $('<li><a data-toggle="collapse" href="#menuMark'+this.id+'"><img class="menu-icon" src="'+this.icon+'"></img>&nbsp;'+this.name+'&nbsp;'+
+		                            '<i class="glyphicon glyphicon-chevron-right pull-right" style="position: relative; right: 12px;font-size: 12px;"></i></a><ul id="menuMark'+this.id+'" class="second-level-menu collapse"></ul></li>');
+		                        $li.appendTo($menu.find('.second-level-menu:first')).find('a').css('padding-left', parseInt(this.level)*18+'px');
+		                        renderSubMenu(this.id, $li);
+		                    }else{
+		                        var $li = $(' <li class="submenu" data-role="openTab" data-target="'+this.identifier+'" data-title="'+this.name+'" ' +
+		                            'data-mark="menuMark'+this.id+'"><a ><img class="menu-icon" src="'+this.icon+'"></img>&nbsp;'+this.name+'</a></li>');
+		                        $li.appendTo($menu.find('.second-level-menu:first')).find('a').css('padding-left', parseInt(this.level)*18+'px');
+		                    }
+						});
+						$menu.find('li.submenu').on('click', function(){
+								var $this = $(this);
+								clearMenuEffect();
+								$this.addClass('active').parent().closest('li').addClass('active').parent().closest('li').addClass('active');
+								var target = $this.data('target');
+								var title = $this.data('title');
+								var mark = $this.data('mark');
+								if(target && title && mark ){
+									$this.openTab(target, title, mark);
+								}
+							});
+				});
+			};
+		});
+	</script>
 </body>
 </html>
