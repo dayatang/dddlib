cf75801158d3a259c503b5e654aa293aacaff752
wangjianbing <wangjb925@gmail.com>
2013-11-27 14:26:17 +0800

    添加树形组件

--- koala-bpm/koala-jbpm-oss/jbpm-oss-web/src/main/webapp/js/processform/core.js
+++ koala-bpm/koala-jbpm-oss/jbpm-oss-web/src/main/webapp/js/processform/core.js
@@ -130,7 +130,7 @@ $(function(){
         				for(var index in data){
         					insertFieldRows(index+1,data[index]);
         				}
-        				$('#formManagement').modal({
+        				$('#formManagement').clone().modal({
                             keyboard: false
                         }).on('hidden.bs.modal', function(){
                         	$(this).remove();
