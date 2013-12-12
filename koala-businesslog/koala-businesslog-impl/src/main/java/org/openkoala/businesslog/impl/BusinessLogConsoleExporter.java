e26821c0c387807672c41742fc862286479e1618
zacker <zacker330@gmail.com>
2013-12-12 11:22:06 +0800

    对渲染与导出日志环节进行重构

--- koala-businesslog/koala-businesslog-impl/src/main/java/org/openkoala/businesslog/impl/BusinessLogConsoleExporter.java
+++ koala-businesslog/koala-businesslog-impl/src/main/java/org/openkoala/businesslog/impl/BusinessLogConsoleExporter.java
@@ -1,17 +1,15 @@
 package org.openkoala.businesslog.impl;
 
-import org.openkoala.businesslog.AbstractBusinessLogExporter;
 import org.openkoala.businesslog.BusinessLogExporter;
 import org.openkoala.businesslog.RenderResult;
 
-import java.util.Map;
 
 /**
  * User: zjzhai
  * Date: 12/8/13
  * Time: 11:49 PM
  */
-public class BusinessLogConsoleExporter extends AbstractBusinessLogExporter {
+public class BusinessLogConsoleExporter implements BusinessLogExporter {
 
 
     @Override
--- koala-businesslog/koala-businesslog-impl/src/main/java/org/openkoala/businesslog/impl/BusinessLogJpaDefaultExporter.java
+++ koala-businesslog/koala-businesslog-impl/src/main/java/org/openkoala/businesslog/impl/BusinessLogJpaDefaultExporter.java
@@ -2,8 +2,7 @@ package org.openkoala.businesslog.impl;
 
 import static org.openkoala.businesslog.common.ContextKeyConstant.*;
 
-import com.dayatang.domain.InstanceFactory;
-import org.openkoala.businesslog.AbstractBusinessLogExporter;
+import org.openkoala.businesslog.BusinessLogExporter;
 import org.openkoala.businesslog.RenderResult;
 import org.openkoala.businesslog.application.BusinessLogApplication;
 import org.openkoala.businesslog.model.SimpleBusinessLog;
@@ -15,7 +14,7 @@ import javax.inject.Inject;
  * Date: 12/5/13
  * Time: 4:52 PM
  */
-public class BusinessLogJpaDefaultExporter extends AbstractBusinessLogExporter {
+public class BusinessLogJpaDefaultExporter implements BusinessLogExporter {
 
     @Inject
     private BusinessLogApplication businessLogApplication;
@@ -24,7 +23,7 @@ public class BusinessLogJpaDefaultExporter extends AbstractBusinessLogExporter {
     public void export(RenderResult renderResult) {
         SimpleBusinessLog log = new SimpleBusinessLog();
         log.setBusinessMethod((String) renderResult.getContext().get(BUSINESS_METHOD));
-        log.setLog(getLog());
+        log.setLog(renderResult.getLog());
         businessLogApplication.save(log);
         assert log.getId() != null;
 
