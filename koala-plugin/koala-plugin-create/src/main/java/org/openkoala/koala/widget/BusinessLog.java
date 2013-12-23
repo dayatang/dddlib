bedede7f17d7859db2a024305456e7774d8dc920
lingen.liu <lingen.liu@gmail.com>
2013-12-23 13:59:02 +0800

    项目创建加入日志子系统的支持

---
+++ koala-plugin/koala-plugin-create/src/main/java/org/openkoala/koala/widget/BusinessLog.java
@@ -0,0 +1,15 @@
+package org.openkoala.koala.widget;
+
+import java.io.Serializable;
+
+import org.openkoala.koala.annotation.ObjectFunctionCreate;
+
+@ObjectFunctionCreate
+public class BusinessLog implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3245235412871983827L;
+
+}
