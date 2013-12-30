315c056b9a6ec866b3b8cef467db80e9243f020f
lingen.liu <lingen.liu@gmail.com>
2013-12-30 10:22:43 +0800

    designer的JAR包提交

--- koala-bpm/koala-jbpm-core/koala-jbpm-core-application/src/main/java/org/openkoala/jbpm/application/AddOnApplication.java
+++
@@ -1,9 +0,0 @@
-package org.openkoala.jbpm.application;
-
-import org.openkoala.jbpm.application.vo.HistoryLogVo;
-
-public interface AddOnApplication {
-	
-	public void jbpmLog(HistoryLogVo log);
-	
-}
