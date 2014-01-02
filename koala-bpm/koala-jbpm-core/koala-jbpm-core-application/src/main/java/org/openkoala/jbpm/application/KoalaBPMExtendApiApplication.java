4ceb5e9e993fde2c20cf7382842188d9c0305986
lingen.liu <lingen.liu@gmail.com>
2014-01-02 21:46:29 +0800

    流程事务并发优化—测试通过

--- koala-bpm/koala-jbpm-core/koala-jbpm-core-application/src/main/java/org/openkoala/jbpm/application/KoalaBPMExtendApiApplication.java
+++ koala-bpm/koala-jbpm-core/koala-jbpm-core-application/src/main/java/org/openkoala/jbpm/application/KoalaBPMExtendApiApplication.java
@@ -1,10 +1,13 @@
 package org.openkoala.jbpm.application;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Koala BPM的扩充实现
  * @author lingen
  *
  */
 public interface KoalaBPMExtendApiApplication {
-
+	
 }
