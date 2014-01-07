c0f174a4283de76205b84d079f471fd83e9a251f
zhuyuanbiao <zhuyuanbiao2013@gmail.com>
2014-01-07 11:35:49 +0800

    加入HTTP处理支持

---
+++ koala-security/koala-security-ss3Adapter/src/main/java/org/openkoala/koala/auth/ss3adapter/AuthenticationFilter.java
@@ -0,0 +1,66 @@
+package org.openkoala.koala.auth.ss3adapter;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * 验证过滤器
+ * @author zyb
+ * @since 2013-04-17 10:37
+ *
+ */
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+	
+	private boolean postOnly = true;
+	
+	private List<HttpHandler> httpHandlers;
+
+	public void setHttpHandlers(List<HttpHandler> httpHandlers) {
+		this.httpHandlers = httpHandlers;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		
+		if (postOnly && !request.getMethod().equals("POST")) {
+			throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+		}
+		
+		for (HttpHandler each : httpHandlers) {
+			each.handle(request, response);
+		}
+		
+        // 获取Username和Password  
+        String username = obtainUsername(request);  
+        String password = obtainPassword(request);  
+  
+        // UsernamePasswordAuthenticationToken实现Authentication校验  
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(  
+                username, password);  
+  
+        // 允许子类设置详细属性  
+        setDetails(request, authRequest);  
+  
+        // 运行UserDetailsService的loadUserByUsername 再次封装Authentication  
+        return this.getAuthenticationManager().authenticate(authRequest);  
+	}
+
+	public boolean isPostOnly() {
+		return postOnly;
+	}
+
+	@Override
+	public void setPostOnly(boolean postOnly) {
+		this.postOnly = postOnly;
+	}
+
+}
---
+++ koala-security/koala-security-ss3Adapter/src/main/java/org/openkoala/koala/auth/ss3adapter/ValidCodeHandler.java
@@ -0,0 +1,68 @@
+package org.openkoala.koala.auth.ss3adapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationServiceException;
+
+import com.octo.captcha.module.servlet.image.SimpleImageCaptchaServlet;
+
+/**
+ * 验证码处理器
+ * @author zhuyuanbiao
+ * @date 2014年1月7日 上午11:11:10
+ *
+ */
+public class ValidCodeHandler implements HttpHandler {
+	
+	private boolean allowEmptyValidateCode = false;
+	
+	private String sessionvalidateCodeField = DEFAULT_SESSION_VALIDATE_CODE_FIELD;
+	
+	public static final String SPRING_SECURITY_LAST_USERNAME_KEY = "SPRING_SECURITY_LAST_USERNAME";
+	
+	// session中保存的验证码
+	public static final String DEFAULT_SESSION_VALIDATE_CODE_FIELD = "rand";
+	
+	// 输入的验证码
+	public static final String DEFAULT_VALIDATE_CODE_PARAMETER = "code";
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response) {
+		
+		checkValidateCode(request);
+	}
+
+	protected String obtainSessionValidateCode(HttpServletRequest request) {
+		Object obj = request.getSession().getAttribute(sessionvalidateCodeField);
+		return null == obj ? "" : obj.toString();
+	}
+
+	public String getValidateCodeName() {
+		return sessionvalidateCodeField;
+	}
+
+	public void setValidateCodeName(String validateCodeName) {
+		this.sessionvalidateCodeField = validateCodeName;
+	}
+
+	public boolean isAllowEmptyValidateCode() {
+		return allowEmptyValidateCode;
+	}
+
+	public void setAllowEmptyValidateCode(boolean allowEmptyValidateCode) {
+		this.allowEmptyValidateCode = allowEmptyValidateCode;
+	}
+	
+	/**
+	 * 比较session中的验证码和用户输入的验证码是否相等
+	 * @param request
+	 */
+	private void checkValidateCode(HttpServletRequest request) {
+		 String userCaptchaResponse = request.getParameter("jcaptcha");  
+		 boolean captchaPassed = SimpleImageCaptchaServlet.validateResponse(request, userCaptchaResponse); 
+	    if (!captchaPassed) {
+			throw new AuthenticationServiceException("Wrong ValidateCode！");
+		}
+	}
+}
