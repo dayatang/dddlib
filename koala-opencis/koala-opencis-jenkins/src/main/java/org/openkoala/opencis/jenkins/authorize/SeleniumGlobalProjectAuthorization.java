42384072009515005cbbc79347ab0e146c8e0a63
zacker <zacker330@gmail.com>
2014-01-09 00:14:09 +0800

    添加全局权限设置

---
+++ koala-opencis/koala-opencis-jenkins/src/main/java/org/openkoala/opencis/jenkins/authorize/SeleniumGlobalProjectAuthorization.java
@@ -0,0 +1,71 @@
+package org.openkoala.opencis.jenkins.authorize;
+
+import org.openkoala.opencis.api.Developer;
+import org.openkoala.opencis.api.Project;
+import org.openkoala.opencis.authorize.CISAuthorization;
+import org.openkoala.opencis.jenkins.util.SeleniumUtil;
+import org.openkoala.opencis.jenkins.util.UrlUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * User: zjzhai
+ * Date: 1/8/14
+ * Time: 11:36 PM
+ */
+public class SeleniumGlobalProjectAuthorization implements CISAuthorization {
+
+    private String jenkinsUrl;
+
+    public SeleniumGlobalProjectAuthorization(String jenkinsUrl) {
+        this.jenkinsUrl = UrlUtil.removeEndIfExists(jenkinsUrl, "/");
+    }
+
+    @Override
+    public void authorize(Project project, Developer developer, Object context) {
+        WebDriver driver;
+        if (context != null) {
+            driver = (WebDriver) context;
+        } else {
+            driver = new HtmlUnitDriver();
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        }
+
+        driver.get(jenkinsUrl + "/configureSecurity/");
+
+        if (SeleniumUtil.elementExist(driver, By.cssSelector("table#hudson-security-ProjectMatrixAuthorizationStrategy tr.permission-row[name=\"[" + developer.getName() + "]\"]"))) {
+            driver.quit();
+            return;
+        }
+
+
+        WebElement usernameInput =
+                driver.findElement(By.cssSelector("table[id=\"hudson-security-ProjectMatrixAuthorizationStrategy\"] +table input[type=\"text\"]"));
+
+        usernameInput.sendKeys(developer.getName());
+
+
+        WebElement addUserSubmitButton = driver.findElement(By.cssSelector("table[id=\"hudson-security-ProjectMatrixAuthorizationStrategy\"] +table button[type=\"button\"]"));
+        addUserSubmitButton.click();
+
+
+        //设置read权限
+        WebElement readPermissionCheckbox = driver.findElement(
+                By.cssSelector("tr.permission-row[name=\"[" + developer.getName() + "]\"] input[name=\"[hudson.model.Hudson.Read]\"][type=\"checkbox\"]"));
+        readPermissionCheckbox.click();
+
+
+        //保存配置
+        WebElement saveButton = driver.findElement(By.cssSelector("span[name=\"Submit\"] button"));
+        saveButton.click();
+
+        assert driver.getCurrentUrl().equals(jenkinsUrl + "/manage");
+
+        //driver.quit();
+
+    }
+}
