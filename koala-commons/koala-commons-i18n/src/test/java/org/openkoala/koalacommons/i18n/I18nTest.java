ede60a6925ef9451eb619c6a71c13a2d0e799bc4
zhuyuanbiao <zhuyuanbiao2013@gmail.com>
2014-01-08 11:19:38 +0800

    修改资源加载方式

--- koala-commons/koala-commons-i18n/src/test/java/org/openkoala/koalacommons/i18n/I18nTest.java
+++ koala-commons/koala-commons-i18n/src/test/java/org/openkoala/koalacommons/i18n/I18nTest.java
@@ -1,8 +1,6 @@
 package org.openkoala.koalacommons.i18n;
 
-import junit.framework.Assert;
-
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openkoala.framework.i18n.I18NManager;
 
@@ -11,18 +9,17 @@ import org.openkoala.framework.i18n.I18NManager;
  * @author lingen
  *
  */
-@Ignore
 public class I18nTest {
 
 	@Test
 	public void shouldUsingDefaultLocale(){
 		String value = I18NManager.getMessage("name");
-        Assert.assertTrue("should using default(zh_CN) locale", value.equals("考拉"));
+		Assert.assertTrue(value.equals("考拉"));
 	}
 	
 	@Test
 	public void shouldUsingENLocale(){
 		String value = I18NManager.getMessage("name","en");
-		Assert.assertTrue("should using en locale", "Koala".equals(value));
+		Assert.assertTrue("Koala".equals(value));
 	}
 }
