ab320e527e658470e15b72d0c1e1cec94ce743e5
lingen.liu <lingen.liu@gmail.com>
2014-01-20 19:21:05 +0800

    Koala提供整合DBUNIT的支持

--- koala-commons/koala-commons-test-support/src/test/java/org/openkoala/test/KoalaDBUnitDataSourceTest.java
+++ koala-commons/koala-commons-test-support/src/test/java/org/openkoala/test/KoalaDBUnitDataSourceTest.java
@@ -10,14 +10,31 @@ import org.springframework.util.Assert;
 
 import com.dayatang.domain.InstanceFactory;
 
-
+/**
+ * 写一个测试类，继承KoalaDataSourceDBUnitTestCase
+ * 默认集成了Koala的Spring IOC环境
+ * IOC的默认配置如下 ：
+ * @Transactional(propagation = Propagation.REQUIRED)
+   @ContextConfiguration(locations = { "classpath*:META-INF/spring/root.xml" })
+   @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+   
+   你可以在本类上，使用上述Annotation,修改默认的这些定义
+ * @author lingen
+ *
+ */
 public class KoalaDBUnitDataSourceTest extends KoalaDataSourceDBUnitTestCase {
 
 	@Override
+	/**
+	 * 指定DBUnit的XML配置文件所在
+	 */
 	public String getDataXmlFile() {
 		return "/dataxml/test.xml";
 	}
 
+	/**
+	 * 指定IOC容器中的DataSourceName
+	 */
 	@Override
 	public String getDataSourceName() {
 		return "dataSource";
@@ -25,6 +42,9 @@ public class KoalaDBUnitDataSourceTest extends KoalaDataSourceDBUnitTestCase {
 	
 	@Test
 	@Ignore
+	/**
+	 * 使用Junit进行测试，你可以方便的使用InstanceFactory进行IOC定义中的BEAN的获取
+	 */
 	public void test(){
 		Assert.isTrue(InstanceFactory.getInstance(DataSource.class)!=null);
 	}
