e9942ffaaf5edf607b53dc278ecf4fa0ecd89dea
yyang <gdyangyu@gmail.com>
2014-02-17 13:41:53 +0800

    给BaseEntity的equals()方法添加注释

--- dddlib-domain/src/test/java/org/dayatang/domain/BaseEntityTest.java
+++ dddlib-domain/src/test/java/org/dayatang/domain/BaseEntityTest.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.dayatang.domain;
 
 import org.dayatang.domain.entity.Company;
@@ -27,112 +26,122 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 /**
- * 
+ *
  * @author yyang
  */
 public class BaseEntityTest {
 
-	private EntityRepository repository;
+    private EntityRepository repository;
 
-	private Organization guangdong = new Company("Guangdong", 1);
+    private final Organization guangdong = new Company("Guangdong", 1);
 
-	private Organization guangzhou = new Company("Guangdong", 2);
+    private final Organization guangzhou = new Company("Guangdong", 2);
 
-	private Organization financial = new Dept("Guangdong", 1);
+    private final Organization financial = new Dept("Guangdong", 1);
 
-	@Before
-	public void setUp() {
-		repository = mock(EntityRepository.class);
-		BaseEntity.setRepository(repository);
-	}
+    @Before
+    public void setUp() {
+        repository = mock(EntityRepository.class);
+        BaseEntity.setRepository(repository);
+    }
 
-	@After
-	public void tearDown() {
-		reset(repository);
-		BaseEntity.setRepository(null);
-	}
+    @After
+    public void tearDown() {
+        reset(repository);
+        BaseEntity.setRepository(null);
+    }
 
-	@Test
-	public void testExistedIdIsNull() {
-		guangdong.setId(null);
-		assertFalse(guangdong.existed());
-	}
+    @Test
+    public void testExistedIdIsNull() {
+        guangdong.setId(null);
+        assertFalse(guangdong.existed());
+    }
 
-	@Test
-	public void testExistedIdIsZero() {
-		guangdong.setId(0);
-		assertFalse(guangdong.existed());
-	}
+    @Test
+    public void testExistedIdIsZero() {
+        guangdong.setId(0);
+        assertFalse(guangdong.existed());
+    }
 
-	@Test
-	public void testExistedRepositoryNotFound() {
-		guangdong.setId(3);
-		when(repository.exists(Organization.class, 3)).thenReturn(false);
-		assertFalse(guangdong.existed());
-	}
+    @Test
+    public void testExistedRepositoryNotFound() {
+        guangdong.setId(3);
+        when(repository.exists(Organization.class, 3)).thenReturn(false);
+        assertFalse(guangdong.existed());
+    }
 
-	@Test
-	public void testExistedRepositoryFound() {
-		guangdong.setId(3);
-		when(repository.exists(Company.class, 3)).thenReturn(true);
-		assertTrue(guangdong.existed());
-	}
+    @Test
+    public void testExistedRepositoryFound() {
+        guangdong.setId(3);
+        when(repository.exists(Company.class, 3)).thenReturn(true);
+        assertTrue(guangdong.existed());
+    }
 
-	@Test
-	public void testNotExistedIdIsNull() {
-		guangdong.setId(null);
-		assertTrue(guangdong.notExisted());
-	}
+    @Test
+    public void testNotExistedIdIsNull() {
+        guangdong.setId(null);
+        assertTrue(guangdong.notExisted());
+    }
 
-	@Test
-	public void testNotExistedIdIsZero() {
-		guangdong.setId(0);
-		assertTrue(guangdong.notExisted());
-	}
+    @Test
+    public void testNotExistedIdIsZero() {
+        guangdong.setId(0);
+        assertTrue(guangdong.notExisted());
+    }
 
-	@Test
-	public void testNotExistedRepositoryNotFound() {
-		guangdong.setId(3);
-		when(repository.exists(Company.class, 3)).thenReturn(false);
-		assertTrue(guangdong.notExisted());
-	}
+    @Test
+    public void testNotExistedRepositoryNotFound() {
+        guangdong.setId(3);
+        when(repository.exists(Company.class, 3)).thenReturn(false);
+        assertTrue(guangdong.notExisted());
+    }
 
-	@Test
-	public void testNotExistedRepositoryFound() {
-		guangdong.setId(3);
-		when(repository.exists(Company.class, 3)).thenReturn(true);
-		assertFalse(guangdong.notExisted());
-	}
+    @Test
+    public void testNotExistedRepositoryFound() {
+        guangdong.setId(3);
+        when(repository.exists(Company.class, 3)).thenReturn(true);
+        assertFalse(guangdong.notExisted());
+    }
 
-	@Test
-	public void testNotExisted() {
-		guangdong.setId(null);
-		assertTrue(guangdong.notExisted());
+    @Test
+    public void testNotExisted() {
+        guangdong.setId(null);
+        assertTrue(guangdong.notExisted());
 
-		guangdong.setId(0);
-		assertTrue(guangdong.notExisted());
+        guangdong.setId(0);
+        assertTrue(guangdong.notExisted());
 
-		guangdong.setId(3);
-		when(repository.exists(Company.class, 3)).thenReturn(false);
-		assertTrue(guangdong.notExisted());
+        guangdong.setId(3);
+        when(repository.exists(Company.class, 3)).thenReturn(false);
+        assertTrue(guangdong.notExisted());
 
-		reset(repository);
-		when(repository.exists(Company.class, 3)).thenReturn(true);
-		assertFalse(guangdong.notExisted());
-	}
+        reset(repository);
+        when(repository.exists(Company.class, 3)).thenReturn(true);
+        assertFalse(guangdong.notExisted());
+    }
 
-	@Test
-	public void testHashCode() {
-		assertEquals(guangdong.hashCode(), new Company("Guangdong", 1).hashCode());
-	}
+    @Test
+    public void testHashCode() {
+        assertEquals(guangdong.hashCode(), new Company("Guangdong", 1).hashCode());
+    }
 
-	@Test
-	public void testEquals() {
-		assertTrue(guangdong.equals(guangdong));
-		assertFalse(guangdong.equals(guangzhou));
-		assertFalse(guangdong.equals(financial));
-		guangdong.disable();
-		assertTrue(guangdong.equals(new Company("Guangdong", 1)));
-	}
+    @Test
+    public void testEquals() {
+        //对象永远等价于自身
+        assertTrue(guangdong.equals(guangdong));
+        
+        //类型相同、业务主键不同的对象不等价
+        assertFalse(guangdong.equals(guangzhou));
+        
+        //类型不同、业务主键相同的对象不等价
+        assertFalse(guangdong.equals(financial));
+        
+        //非业务主键不影响等价性
+        guangdong.disable();
+        assertTrue(guangdong.equals(new Company("Guangdong", 1)));
+        
+        //如果a.equals(b)，那么b.equals(a)
+        assertTrue(new Company("Guangdong", 1).equals(guangdong));
+    }
 
 }
