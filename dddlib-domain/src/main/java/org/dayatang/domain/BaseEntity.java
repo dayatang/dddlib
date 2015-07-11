7ba9ca9d0c154ee5a6ed7547be1f62e51d87f75a
yyang <gdyangyu@gmail.com>
2015-07-11 19:40:51 +0800

    删除过时内容

--- dddlib-domain/src/main/java/org/dayatang/domain/BaseEntity.java
+++ dddlib-domain/src/main/java/org/dayatang/domain/BaseEntity.java
@@ -75,48 +75,4 @@ public abstract class BaseEntity implements Entity {
     public String[] businessKeys() {
         return new String[] {};
     }
-
-    /**
-     * 依据业务主键获取哈希值。用于判定两个实体是否等价。
-     * 等价的两个实体的hashCode相同，不等价的两个实体hashCode不同。
-     * @return 实体的哈希值
-     */
-    @Override
-    public int hashCode() {
-        HashCodeBuilder builder = new HashCodeBuilder(13, 37);
-        Map<String, Object> propValues = new BeanUtils(this).getPropValues();
-        
-        for (String businessKey : businessKeys()) {
-            builder = builder.append(propValues.get(businessKey));
-        }
-        return builder.toHashCode();
-    }
-
-    /**
-     * 依据业务主键判断两个实体是否等价。
-     * @param other 另一个实体
-     * @return 如果本实体和other等价则返回true,否则返回false
-     */
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) {
-            return true;
-        }
-        if (other == null) {
-            return false;
-        }
-        if (businessKeys() == null || businessKeys().length == 0) {
-            return false;
-        }
-        if (!(this.getClass().isAssignableFrom(other.getClass()))) {
-            return false;
-        }
-        Map<String, Object> thisPropValues = new BeanUtils(this).getPropValuesExclude(Transient.class);
-        Map<String, Object> otherPropValues = new BeanUtils(other).getPropValuesExclude(Transient.class);
-        EqualsBuilder builder = new EqualsBuilder();
-        for (String businessKey : businessKeys()) {
-            builder.append(thisPropValues.get(businessKey), otherPropValues.get(businessKey));
-        }
-        return builder.isEquals();
-    }
 }
