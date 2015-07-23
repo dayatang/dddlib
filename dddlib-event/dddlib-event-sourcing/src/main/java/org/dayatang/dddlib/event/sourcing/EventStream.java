23fb886ccde9441d0a8e41ea3c8c092686820d40
yyang <gdyangyu@gmail.com>
2015-07-23 16:36:02 +0800

    升级插件版本，将site deploy机制从webdav改为ssh

---
+++ dddlib-event/dddlib-event-sourcing/src/main/java/org/dayatang/dddlib/event/sourcing/EventStream.java
@@ -0,0 +1,7 @@
+package org.dayatang.dddlib.event.sourcing;
+
+/**
+ * Created by yyang on 15/7/23.
+ */
+public class EventStream {
+}
---
+++ dddlib-event/dddlib-event-storage/src/main/java/org/dayatang/dddlib/event/storage/EventStorageRedis.java
@@ -0,0 +1,14 @@
+package org.dayatang.dddlib.event.storage;
+
+import org.dayatang.dddlib.event.api.Event;
+import org.dayatang.dddlib.event.api.EventStore;
+
+/**
+ * Created by yyang on 15/7/23.
+ */
+public class EventStorageRedis implements EventStore {
+    @Override
+    public void store(Event event) {
+
+    }
+}
