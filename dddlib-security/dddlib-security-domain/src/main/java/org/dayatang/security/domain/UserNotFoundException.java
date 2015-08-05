8158e14a273c22a18d3def0ba48721921fd92562
yyang <gdyangyu@gmail.com>
2015-08-05 10:44:14 +0800

    完善security模块

---
+++ dddlib-security/dddlib-security-domain/src/main/java/org/dayatang/security/domain/UserNotFoundException.java
@@ -0,0 +1,25 @@
+package org.dayatang.security.domain;
+
+/**
+ * Created by yyang on 15/8/5.
+ */
+public class UserNotFoundException extends SecurityException {
+    public UserNotFoundException() {
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
