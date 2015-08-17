313543b41cedc436a79bafbaab786fcaaf8e9ec8
yyang <gdyangyu@gmail.com>
2015-08-17 09:37:35 +0800

    完善权限模块

--- dddlib-examples/dddlib-examples-organisation/organisation-security/src/main/java/org/dddlib/organisation/security/domain/OrgAuthorizationScope.java
+++ dddlib-examples/dddlib-examples-organisation/organisation-security/src/main/java/org/dddlib/organisation/security/domain/OrgAuthorizationScope.java
@@ -3,6 +3,8 @@ package org.dddlib.organisation.security.domain;
 import org.dayatang.security.domain.AuthorizationScope;
 import org.dddlib.organisation.domain.Organization;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
@@ -14,6 +16,8 @@ import java.util.Set;
  * 机构授权范围。由一个机构代表的授权范围。机构间的直线管理关系映射到授权范围的包含与被包含关系
  * Created by yyang on 15/8/17.
  */
+@Entity
+@DiscriminatorValue("ORG")
 public class OrgAuthorizationScope extends AuthorizationScope {
 
     @NotNull
