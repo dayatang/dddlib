10fbe1a8640f4a75372a9c222b6dc613914d4e7c
yyang <gdyangyu@gmail.com>
2016-12-21 20:15:14 +0800

    update

---
+++ dddlib-codegen/dddlib-codegen-engine/src/main/java/org/dddlib/codegen/engine/PackageDefinition.java
@@ -0,0 +1,26 @@
+package org.dddlib.codegen.engine;
+
+/**
+ * Created by yyang on 2016/12/21.
+ */
+public class PackageDefinition {
+    private String name;
+    private String description;
+
+    public PackageDefinition(String name) {
+        this.name = name;
+    }
+
+    public PackageDefinition(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
