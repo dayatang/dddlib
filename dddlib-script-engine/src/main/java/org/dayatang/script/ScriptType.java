ced9366fca4aa34020cf86c69581579ea6c12b42
yyang <gdyangyu@gmail.com>
2020-11-14 19:33:44 +0800

    去除脚本引擎模块

--- dddlib-script-engine/src/main/java/org/dayatang/script/ScriptType.java
+++
@@ -1,9 +0,0 @@
-package org.dayatang.script;
-
-public interface ScriptType {
-
-	public static final String GROOVY = "groovy";
-	
-	public static final String JS = "js";
-
-}
--- dddlib-script-engine/src/main/java/org/dayatang/script/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * 脚本引擎服务
- * Created by yyang on 14-2-5.
- */
-package org.dayatang.script;
